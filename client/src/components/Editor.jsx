2656c37320666e786817060a9e78bb1694f25786
Harsha-10 <dharsha9949@gmail.com>
2023-12-06 19:35:02 +0530

    io url for client

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -39,7 +39,7 @@ const Editor = () => {
         setQuill(quill);
     }, [])
     useEffect(()=>{
-        const socketServer = io('');
+        const socketServer = io('https://google-docs-virid.vercel.app');
         setSocket(socketServer);
         return ()=>{
             socketServer.disconnect(); 
