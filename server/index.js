3061fdf5176960312b8a73f26e395750e250e4e1
Harsha-10 <dharsha9949@gmail.com>
2023-12-06 22:55:07 +0530

    corrected server

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://google-docs-virid.vercel.app",
+    origin: httpServer,
     methods: ["GET", "POST"],
     credentials: true,
   },
