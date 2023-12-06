ccf8a31f9f2910c5fdd7c6ab35c3a70a747aaf75
Harsha-10 <dharsha9949@gmail.com>
2023-12-06 17:21:09 +0530

    updated the code for production build

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose'
-const connect = async(username='docsuser',password='docsuser123')=>{
-    const url = `mongodb+srv://${username}:${password}@cluster0.lb8gg2f.mongodb.net/google-docs?retryWrites=true&w=majority`;
+const connect = async(url)=>{
     try {
         await mongoose.connect(url);
         console.log("db connected");
---
+++ server/server.js
@@ -0,0 +1,41 @@
+import { Server } from "socket.io";
+import connect from "./db/database.js";
+import Document from "./schema/schema.js";
+const url = process.env.MONGODB_URI || `mongodb+srv://docsuser:docsuser123@cluster0.lb8gg2f.mongodb.net/google-docs?retryWrites=true&w=majority`;
+connect(url);
+
+const io = new Server(http,{
+cors: {
+    origin: 'https://google-docs-backend.vercel.app',
+    methods: ['GET', 'POST'],
+    credentials: true
+}
+});
+
+io.on('connection', socket => {
+socket.on('get-doc', async documentID => {
+    const doc = await getDocument(documentID);
+    socket.join(documentID);
+    socket.emit('load-doc', doc.data);
+    console.log(doc);
+
+    socket.on('send-changes', delta => {
+    socket.broadcast.to(documentID).emit('recieve-changes', delta);
+    });
+
+    socket.on('save', async newData => {
+        try {
+            await Document.findByIdAndUpdate(documentID, { data: newData });
+            } catch (error) {
+                console.error("Error saving document:", error);
+            }
+        });
+});
+});
+
+const getDocument = async (id) => {
+if (id == null) return;
+const document = await Document.findById(id);
+if (document) return document;
+return await Document.create({ _id: id, data: "" });
+};
