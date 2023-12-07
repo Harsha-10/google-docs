d7161774517b0cb40b93758518ca6303217513a8
Harsha-10 <dharsha9949@gmail.com>
2023-12-07 14:55:12 +0530

    added transport ,ethods

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -41,7 +41,7 @@ const Editor = () => {
     useEffect(()=>{
         const socketServer = io('https://google-docs-server-zeta.vercel.app', {
             withCredentials: true,
-            transports : ['websocket']
+            transports : ['websocket', 'polling', 'streaming']
         });
         setSocket(socketServer);
         return ()=>{
